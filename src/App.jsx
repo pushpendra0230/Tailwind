c346a0f6ad4d5e48e2683d2b18e232572a0361fc
pushpendra0230 <153480201+pushpendra0230@users.noreply.github.com>
2024-11-27 16:33:24 +0530

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
-const App = () => {
-  return (
-    <div>
-      <h1 className="bg-orange-400">Hello, world!</h1>
-    </div>
-  );
-  
-};
-export default App;
+// // q.1.Create 5 divs and shift the to the top using Tailwind.
+
+// import React from 'react';
+// const App = () => {
+//   return (
+//     <div>
+//       <div className="my-8 bg-red-300 p-4">
+//         <h1>Div 1</h1>
+//       </div>
+//       <div className="bg-blue-300 p-4">
+//         <h1>Div 2</h1>
+//       </div>
+//       <div className="bg-green-300 p-4">
+//         <h1>Div 3</h1>
+//       </div>
+//       <div className="bg-yellow-300 p-4">
+//         <h1>Div 4</h1>
+//       </div>
+//       <div className="bg-purple-300 p-4">
+//         <h1>Div 5</h1>
+//       </div>
+//     </div>
+//   );
+// };
+// export default App;
+
+// q.2.Create 4 divs and shift the to the top.
+
+// import React from 'react';
+// const App = () => {
+//   return (
+//     <div>
+//       <div className="my-8 bg-red-500">
+//         <h1>Div 1</h1>
+//         </div>
+//         <div className="-mt-8 bg-blue-500">
+//           <h1>Div 2</h1>
+//         </div>
+//         <div className="-mt8 bg-green-500">
+//           <h1>Div 3</h1>
+//         </div>
+//         <div className="-mt8 bg-yellow-500">
+//           <h1>Div 4</h1>
+//         </div>
+//     </div>
+//   );
+// };
+// export default App;
