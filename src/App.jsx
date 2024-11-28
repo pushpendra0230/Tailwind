5b331f42cd8e513e9b87d157233d0a245a1212cf
pushpendra0230 <153480201+pushpendra0230@users.noreply.github.com>
2024-11-28 12:29:51 +0530

    js

--- src/App.jsx
+++ src/App.jsx
@@ -46,3 +46,112 @@
 //   );
 // };
 // export default App;
+
+// import React from "react";
+
+// const App = () => {
+//   return (
+//     <>
+//       <div className="bg-red-200 p-8 mt-[60px]">1</div>
+//       <div className="bg-red-300 p-8">2</div>
+//       <div className="bg-red-400 p-8">3</div>
+//       <div className="bg-red-500 p-8">5</div>
+//       <div className="bg-red-600 p-8 -mt-[26rem]">4</div>
+//     </>
+//   );
+// };
+
+// export default App;
+
+// import React from "react";
+
+// const App = () => {
+//   return (
+//     <>
+//       <header>
+//         <div className="bg-red-300 p-10">1</div>
+//       </header>
+//       <footer>
+//         <div className="bg-green-500 p-28 mt-[480px]">2</div>
+//       </footer>
+//     </>
+//   );
+// };
+
+// export default App;
+
+
+// import React from "react";
+
+// const App = () => {
+//   return (
+//     <>
+//       <button
+//         className="bg-red-400 p-3 m-5 w-36 rounded-3xl text-sm text-[#f7ff12] lg:text-xl lg:bg-green-400"
+//       >
+//         Submit
+//       </button>
+//     </>
+//   );
+// };
+
+// export default App;
+
+
+// import React from "react";
+
+// const App = () => {
+//   return (
+//     <>
+//       <button className="bg-green-500 text-white px-4 py-2 m-8 rounded hover:bg-green-t active:bg-green">
+//         Hover and Active Button
+//       </button>
+//     </>
+//   );
+// };
+
+// export default App;
+
+
+// import React from "react";
+
+// const App = () => {
+//   return (
+//     <>
+//       <button className="border m-8 border-blue-500 text-blue-500 px-4 py-2 rounded hover:bg-blue-500 hover:text-white">
+//         Outline Button
+//       </button>
+//     </>
+//   );
+// };
+// export default App;
+
+
+// import React from "react";
+
+// const App = () => {
+//   return (
+//     <>
+//       <button className="bg-purple-500 text-white px-4 py-2 rounded w-full">
+//         Full-Width Button
+//       </button>
+//     </>
+//   );
+// };
+
+// export default App;
+
+
+// import React from "react";
+
+// const App = () => {
+//   return (
+//     <>
+//       <button className="bg-purple-700 text-white px-4 py-2 m-8 rounded hover:border-solid hover:border-cyan-500 hover:border-4">
+//         Full-Width Button
+//       </button>
+//     </>
+//   );
+// };
+
+// export default App;
